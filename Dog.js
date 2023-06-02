61572f444ab7b992f7066819965d6961b4943064
Anastasiia <nastena.stuzhuk@gmail.com>
2023-06-02 10:30:13 +0300

    Added class Dog

--- Dog.js
+++ Dog.js
@@ -1,1 +1,18 @@
-// Create the Dog class here
+class Dog {
+  constructor(data) {
+    Object.assign(this, data)
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this
+    return `
+      <div>
+        <img src="${avatar}" width="534" height="750" alt="The photo of dog">
+        <div>${name}, ${age}</div>
+        <div>${bio}</div>
+      </div>
+    `
+  }
+}
+
+export default Dog
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Remember to import the data and Dog class!
+import dogs from './data.js'
+import Dog from './Dog.js'
+
+function getNewDog() {
+  const nextDogData = dogs.shift()
+  return nextDogData ? new Dog(nextDogData) : {}
+}
+
+const render = () => 
+  document.body.innerHTML = dog.getDogHtml()
+
+let dog = getNewDog()
+render()
+
+
